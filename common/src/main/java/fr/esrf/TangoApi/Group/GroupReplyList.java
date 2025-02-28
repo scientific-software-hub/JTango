888c8fb778967d7b2fa5b48b2d0568b99ec81386
ingvord <ingvord.mail@gmail.com>
2025-02-28 21:23:47 +0100

    Fix SonarCould reported issue

--- common/src/main/java/fr/esrf/TangoApi/Group/GroupReplyList.java
+++ common/src/main/java/fr/esrf/TangoApi/Group/GroupReplyList.java
@@ -54,7 +54,7 @@ public class GroupReplyList extends Vector implements java.io.Serializable {
     }
     
     /** Adds an element to the list */
-    public boolean add(Object o) {
+    public synchronized boolean add(Object o) {
         if (o instanceof GroupReply == false) {
             return true;
         }
